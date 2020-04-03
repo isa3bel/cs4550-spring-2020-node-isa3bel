40a26e8aa55dceb71d2fef6af56dbee681ca9cd3
Isabel B <isabelbolger@hotmail.com>
2020-04-03 13:54:43 -0700

    deploy 6

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-      "http://localhost:4200");
+      "https://cs4550-isabelb-angular-hw8.herokuapp.com/");
   res.header("Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
